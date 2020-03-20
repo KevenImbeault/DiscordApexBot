51f41ca38fa3e085be0d2b6d3bc67d8df5acd157
Keven Imbeault <imbeault.keven@gmail.com>
2020-03-20 15:09:06 -0400

    Added Apex API calls to get player data

--- CalveryApexBot/CalveryApexBot/Commands.cs
+++ CalveryApexBot/CalveryApexBot/Commands.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,14 +11,20 @@ using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.CommandsNext.Converters;
 using DSharpPlus.Entities;
 using DSharpPlus.Interactivity;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace CalveryApexBot
 {
     public class Commands
     {
+        private RestClient client = new RestClient("https://public-api.tracker.gg/v2/apex/standard/profile/");
+        
+
         [Command("verifyme")]
-        public async Task Verify(CommandContext ctx)
+        public async Task Verify(CommandContext ctx, String username)
         {
+            string platform = "";
             var interactivity = ctx.Client.GetInteractivityModule();
             //Create array of the three used emojis for selecting platforms
             DiscordEmoji[] emojis = { DiscordEmoji.FromUnicode("🧡"), DiscordEmoji.FromUnicode("💚"), DiscordEmoji.FromUnicode("💙")};
@@ -33,7 +41,58 @@ namespace CalveryApexBot
                 //Discord's API of 2 actions per second
                 Thread.Sleep(500);
             }
-                
+
+            var userReaction = await interactivity.WaitForReactionAsync(xe => Array.Exists(emojis, x => x == xe), ctx.User, TimeSpan.FromMinutes(1));          
+            
+            if(userReaction != null)
+            {
+                switch(userReaction.Emoji.Name)
+                {
+                    case "🧡":
+                        platform = "origin";
+                        await ctx.RespondAsync($"Origin {username}");
+                        break;
+                    case "💚":
+                        platform = "xbl";
+                        await ctx.RespondAsync($"Xbox {username}");
+                        break;
+                    case "💙":
+                        platform = "psn";
+                        await ctx.RespondAsync($"Playstation {username}");
+                        break;
+                };
+            } else
+            {
+                await ctx.RespondAsync("didn't work");
+            }
+
+            var tcs = new TaskCompletionSource<string>();
+
+            try
+            {
+                RestRequest request = new RestRequest($"{platform}/{username}", Method.GET);
+                request.AddHeader("TRN-Api-Key", "76ce319e-45b9-4876-9193-7756bfcf609c");
+                request.RequestFormat = DataFormat.Json;
+
+                client.GetAsync(request, (response, handle) =>
+                {
+                    if ((int)response.StatusCode >= 400)
+                    {
+                        tcs.SetException(new Exception(response.StatusDescription));
+                    }
+                    else
+                    {
+                        tcs.SetResult(response.Content);
+                    }
+
+                });
+            } catch(Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+
+            var userData = await tcs.Task;
+
         }
     }
 }
