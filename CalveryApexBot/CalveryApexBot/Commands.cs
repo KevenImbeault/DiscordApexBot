16836f30d956cb7d514f0116bf518104c81b9108
Keven Imbeault <imbeault.keven@gmail.com>
2020-03-20 12:41:21 -0400

    initial commit

---
+++ CalveryApexBot/CalveryApexBot/Commands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+
+namespace CalveryApexBot
+{
+    public class Commands
+    {
+        [Command("verifyme")]
+        public async Task Verify(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivityModule();
+            DiscordEmoji[] emojis = { DiscordEmoji.FromUnicode("🧡"), DiscordEmoji.FromUnicode("💚"), DiscordEmoji.FromUnicode("💙")};
+
+            var msg = await ctx.RespondAsync($"Hey, {ctx.User.Mention}!\n" +
+                "What platform do you play on ?\n" +
+                "Respond with one of the following emojis\n" +
+                ":orange_heart: - Origin  :green_heart: - Xbox  :blue_heart: - Playstation");
+
+            for (var i = 0; i < emojis.Length; ++i)
+            {
+                await msg.CreateReactionAsync(emojis[i]);
+                Thread.Sleep(500);
+            }
+                
+        }
+    }
+}
---
+++ CalveryApexBot/CalveryApexBot/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+
+namespace CalveryApexBot
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+        static InteractivityModule interactivity;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                //Searches the Windows Environement Variables for the token
+                Token = Environment.GetEnvironmentVariable("token", EnvironmentVariableTarget.Machine),
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "."
+            });;
+
+            commands.RegisterCommands<Commands>();
+            interactivity = discord.UseInteractivity(new InteractivityConfiguration
+            {
+                // default pagination behaviour to just ignore the reactions
+                PaginationBehaviour = TimeoutBehaviour.Ignore,
+
+                // default pagination timeout to 5 minutes
+                PaginationTimeout = TimeSpan.FromMinutes(5),
+
+                // default timeout for other actions to 2 minutes
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
